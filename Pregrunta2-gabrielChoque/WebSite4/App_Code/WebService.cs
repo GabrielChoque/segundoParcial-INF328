6097b3d1c6b119e79a097cb70835786da4f14f05
GabrielChoque <66540960+GabrielChoque@users.noreply.github.com>
2021-05-30 19:29:05 -0400

    Add files via upload

---
+++ Pregrunta2-gabrielChoque/WebSite4/App_Code/WebService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data;
+using System.Data.SqlClient;
+/// <summary>
+/// Descripción breve de WebService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+// [System.Web.Script.Services.ScriptService]
+public class WebService : System.Web.Services.WebService {
+
+    public WebService () {
+
+        //Elimine la marca de comentario de la línea siguiente si utiliza los componentes diseñados 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld() {
+        return "Hola a todos";
+    }
+
+    [WebMethod]
+    public DataSet alumno(int bci)
+    {
+
+        SqlConnection con = new SqlConnection();
+        SqlDataAdapter ada = new SqlDataAdapter();
+        con.ConnectionString = "server=(local);user=sa;pwd=123456;database=materias";
+        ada.SelectCommand = new SqlCommand();
+        ada.SelectCommand.CommandText = "select A.nombre,A.apellido,M.nombreM from alumnoE A, materiaE M , asignacion S where A.ci = S.ci and S.id = M.id and A.ci = " + bci;
+        ada.SelectCommand.CommandType = CommandType.Text;
+        ada.SelectCommand.Connection = con;
+        DataSet ds = new DataSet();
+        ada.Fill(ds);
+        return ds;
+    }
+    
+}
---
+++ Pregrunta2-gabrielChoque/WebSite4/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        ServiceReference1.WebServiceSoapClient sw = new ServiceReference1.WebServiceSoapClient();
+        this.GridView1.DataSource = sw.alumno(0).Tables[0];
+        this.GridView1.DataBind();
+
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        ServiceReference1.WebServiceSoapClient sw = new ServiceReference1.WebServiceSoapClient();
+        this.GridView1.DataSource = sw.alumno(Int32.Parse(TextBox1.Text)).Tables[0];
+        this.GridView1.DataBind();
+
+    }
+}
